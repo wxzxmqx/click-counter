38a91b2d9b08c63782da9ff259fae5a9a5bf0849
wxzxmqx <yatsunenko.serhii@hneu.net>
2025-12-14 14:45:37 +0100

    feat: implement theme class synced with js

--- index.php
+++ index.php
@@ -9,10 +9,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Click Counter</title>
 </head>
-<body>
+<body class="light"> 
     <?php include "includes/header.php"; ?>
 
     <main>
+        <section id="welcome">
+            <h1>Click Speed Test</h1>
+            <p>Click as much as you can before time runs out</p>
+        </section>
         <section id="timer">
             <h2 class="current-timer">5</h2> 
             <p>seconds</p>
